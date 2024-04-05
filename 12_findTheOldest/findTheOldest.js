73358dd4fce24e553613951587c79c154cc5f887
Kr-Abdou <abdoukermiche07@gmail.com>
2024-04-05 03:41:44 +0000

    complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let old = 0;
+    let oldestName = "";
+    for(let ppl of people){
+        if(!ppl.yearOfDeath){
+            ppl.yearOfDeath = new Date().getFullYear();
+        }
+        const age = ppl.yearOfDeath - ppl.yearOfBirth;
+        if (age > old ){
+            old = age
+            oldestName = ppl.name;
+        }
+    }
+    return  { name: oldestName }; ;
 };
 
 // Do not edit below this line
